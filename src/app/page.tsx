ac597897a5b06aa56f4044d49c9b5bfe87e8e67b
AndersonArnold <anderson.arnold333@gmail.com>
2026-03-23 19:25:13 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
     }
   };
 
-  const handleCheckout = () => {
+  const handleCheckout = async () => {
     if (!config.actualIsOpen) {
       alert("Desculpe, a loja está fechada no momento.");
       return;
