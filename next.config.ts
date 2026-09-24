bd6eb20539950e80f048d8ab6d85b9cc010f1581
AndersonArnold <anderson.arnold333@gmail.com>
2026-09-24 16:37:53 -0300

    Simplify image configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,27 +3,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-
-  images: {
-
-    remotePatterns: [
-
-      {
-
-        protocol: 'https',
-
-        hostname: 'edzcezjkshefeotgtxnt.supabase.co',
-
-        port: '',
-
-        pathname: '/storage/v1/object/public/product-images/**',
-
-      },
-
-    ],
-
-  },
-
+  images: { unoptimized: true },
 };
 
 
