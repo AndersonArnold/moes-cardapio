8f4900c3653e63506b719318684645a8d478c3e8
AndersonArnold <anderson.arnold333@gmail.com>
2026-09-24 16:18:37 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,31 @@
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
+
   images: {
+
     remotePatterns: [
+
       {
+
         protocol: 'https',
+
         hostname: 'edzcezjkshefeotgtxnt.supabase.co',
+
         port: '',
+
         pathname: '/storage/v1/object/public/product-images/**',
+
       },
+
     ],
+
   },
-  async rewrites() {
-    return [
-      {
-        source: '/storage-cdn/:path*',
-        destination: 'https://edzcezjkshefeotgtxnt.supabase.co/storage/v1/object/public/:path*',
-      },
-    ];
-  },
+
 };
+
+
 
 export default nextConfig;
